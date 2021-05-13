b6eb0f6a5a7b759a85feb09d7e561a940e70a47e
Femi <ologunwafemi@gmail.com>
2021-05-13 03:00:12 +0100

    Updates

---
+++ precache-manifest.84dba1e014b8ffe837c0e1b6042e2462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41784b45d80128c674eafa1b266bb6aa",
+    "url": "/tours-project/index.html"
+  },
+  {
+    "revision": "2f6d21378eb40f9adb7b",
+    "url": "/tours-project/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "367d3ad82c25432129b5",
+    "url": "/tours-project/static/js/2.0bb548be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-project/static/js/2.0bb548be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6d21378eb40f9adb7b",
+    "url": "/tours-project/static/js/main.7a19aeea.chunk.js"
+  },
+  {
+    "revision": "0cdf43e10068da23e0d0",
+    "url": "/tours-project/static/js/runtime-main.53f2128a.js"
+  }
+]);
